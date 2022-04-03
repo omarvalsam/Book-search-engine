72ae689a176809931deeef98037b8cc056369eef
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-04-03 14:48:38 -0600

    added more fixes to client queries, mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+
+export const USER = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
