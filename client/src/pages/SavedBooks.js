2eb0697a220509d4afbf14bf4f6dfa674a82b9e0
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-04-05 11:40:32 -0600

    DEPLOYING

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -15,7 +15,7 @@ import Auth from "../utils/auth";
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
   const userData = data?.me || [];
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
